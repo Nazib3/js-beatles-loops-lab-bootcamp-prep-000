d2226526f091b34cf2eb1289b8a31f78095717b5
Nazibul Haque <elegant.nazib@gmail.com>
2018-03-11 09:15:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ function theBeatlesPlay(arm, ari) {
   }
   return beatles;
 }
+function johnLennonFacts(facts) {
+  var arr = [];
+  var i = 0;
+  while (i < facts.length) {
+    arr.push(`${facts[i]}!!!`)
+    i++
+  }
+  return arr;
+}
